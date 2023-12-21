399532ccda23489f449622dac7e7d722903907b0
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2023-12-21 11:03:41 +0000

    conf: Add extensions

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,20 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+]
+
+intersphinx_mapping = {
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
+intersphinx_disabled_domains = ["std"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -46,6 +59,8 @@ master_doc = 'index'
 project = 'Linux Mint Developer Guide'
 copyright = '2017, Linux Mint'
 author = 'Linux Mint'
+
+
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
